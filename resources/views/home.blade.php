b5307d8415d7ae0e044f880b69d30d38a9bc8345
ayaxxchanz <aliya.genius@gmail.com>
2020-12-13 21:55:13 +0800

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-2 px-5">
+            <img src="https://instagram.fkul6-1.fna.fbcdn.net/v/t51.2885-19/10724055_300191290170380_1081923520_a.jpg?_nc_ht=instagram.fkul6-1.fna.fbcdn.net&_nc_ohc=CT-sygVTGX4AX_H6eGs&oh=bbc1076529181a36fb233215e0b3d250&oe=6000F778" class="rounded-circle" style="border: 1px solid #e4e3e1">
+        </div>
+        <div class="col-9">
+            <div>
+                <h2>Aliya</h2>
+            </div>
+            <div class="d-flex pt-4">
+                <div class="pr-5"><strong>4</strong> posts</div>
+                <div class="pr-5"><strong>165</strong> followers</div>
+                <div class="pr-5"><strong>176</strong> following</div>
+            </div>
+            <div class="pt-3"><strong>Aya</strong></div>
+            <div>I am a programmer.</div>
+            <div><a href="#">ayaxxchanz.github.io</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://instagram.fkul6-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/129737491_2784437911770572_2287274853812690657_n.jpg?_nc_ht=instagram.fkul6-2.fna.fbcdn.net&_nc_cat=108&_nc_ohc=LjwiufGp3rwAX8SaECZ&tp=1&oh=9482507c4a8bb816d562f1c1f0ff1923&oe=60005EEF" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fkul6-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/130349231_2953653378212423_3302295236496904081_n.jpg?_nc_ht=instagram.fkul6-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=iWd6_OL9ITsAX9M7qJ7&tp=1&oh=d7d4e7a1e4af22c60751cdce1ddaa4c3&oe=5FFE6722" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fkul6-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/128552279_176631784125678_1691543487271656856_n.jpg?_nc_ht=instagram.fkul6-1.fna.fbcdn.net&_nc_cat=1&_nc_ohc=7T08K-2-co8AX9sHHkk&tp=1&oh=57eace74d74694c6e6240370d9d03f12&oe=5FFE7E1D" class="w-100">
+        </div>
+    </div>
+</div>
+@endsection
