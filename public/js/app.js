e25db2ef361efb536716a896897c99b15ebc3508
ayaxxchanz <aliya.genius@gmail.com>
2021-12-26 13:41:07 +0800

    minor fix & ansi-html fix

--- public/js/app.js
+++ public/js/app.js
@@ -49907,8 +49907,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\work\laravel\mysite\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\work\laravel\mysite\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\Work\Laravel\MySite\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! E:\Work\Laravel\MySite\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
