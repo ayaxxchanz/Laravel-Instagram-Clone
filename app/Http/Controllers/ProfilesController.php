1fd97f6b0936dd0ded145d9abf48fd271200c788
ayaxxchanz <aliya.genius@gmail.com>
2021-12-27 21:08:13 +0800

    fix vue.js error

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
-    public function index()
+    public function index($id)
     {
-        return view('home');
+        $user = User::find($id);
+        return view('home', [
+            'username' => $user
+        ]);
     }
 }
