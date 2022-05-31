5e573cfa6ba514a259008e986ec5b78603bc2910
Aya <aliya.genius@gmail.com>
2022-05-31 14:58:17 +0800

    fix: 404 error

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/p/{post}', [App\Http\Controllers\PostsController::class, 'show']);
 Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create']);
 Route::post('/p', [App\Http\Controllers\PostsController::class, 'store']);
+Route::get('/p/{post}', [App\Http\Controllers\PostsController::class, 'show']);
 
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
