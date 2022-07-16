86a0e90df4a2d9e35bc74fc60b79eed56a943299
Aya <aliya.genius@gmail.com>
2022-07-17 00:42:45 +0800

    Feat: Attach/detach profile

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -9,6 +9,7 @@ class FollowsController extends Controller
 {
     public function store(User $user)
     {
-        return $user->username;
+        //Attach and detach user's profile
+        return auth()->user()->following()->toggle($user->profile);
     }
 }
